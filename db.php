0c6e506b44dd932ae8d2c90638a867ec88714a0f
samb33t <sambeet96@gmail.com>
2022-03-17 04:15:00 +0100

    Adding DB to login page

--- db.php
+++ db.php
@@ -2,10 +2,9 @@
     $db_host = "localhost";
     $db_user = "root";
     $db_pass = "root";
+    $db_name = "chat";
 
-    $conn = new mysqli($db_host, $db_user, $db_pass);
-    if ($conn->connect_error) {
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+    if ($conn->connect_error)
         die("Connection failed: " . $conn->connect_error);
-      }
-      echo "Connected successfully";
 ?>
--- login.php
+++ login.php
@@ -1,15 +1,21 @@
 <?php
     session_start();
+    include("db.php");
     if(isset($_POST["login"])){
-        $username = $_POST["username"];
-        $password = $_POST["password"];
+        $username = mysqli_real_escape_string($conn, $_POST["username"]);
+        $password = mysqli_real_escape_string($conn, $_POST["password"]);
+        if(strlen($username)==0 || strlen($password)==0)
+            header("Location:login.php?err=1");
+        $query = "SELECT * FROM login WHERE username='$username' AND password='$password'";
+        $result = $conn->query($query);
+        $row_count = $result->num_rows;
+        if($row_count==0)
+            header("Location:login.php?err=2");
+        else{
+            $_SESSION["username"] = $username;
+            header("Location:index.php");
+        }
     }
-
-    if($username == "test" && $password="test"){
-        $_SESSION["username"] = $username;
-        header("Location:index.php");
-    }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,14 +23,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>kickAss Chat</title>
+    <title>kickAss Chat!</title>
 </head>
 <body>
     <h1>kickAss Chat login!</h1>
     <?php
         if(isset($_GET["err"])){
+            $error_id = $_GET["err"];
+            if($error_id==0)    
                 echo("You need to login to see this page!<br><br>");
-            }
+            elseif($error_id==1)
+                echo("Username/ Password cannot be blank!<br><br>");
+            elseif($error_id==2)
+                echo("Username/ Password is wrong!<br><br>");
+        }
     ?>
     <form action="#" method="post">
         <label>Username</label><br> 
