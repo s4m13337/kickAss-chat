55a8a1acc9fe0f9382af9d1c452c820d989d4f90
samb33t <sambeet96@gmail.com>
2022-03-22 14:46:12 +0100

    Added script to change user status on logout

--- logout.php
+++ logout.php
@@ -1,5 +1,11 @@
 <?php
     session_start();
+
+    // Update user status
+    include_once("db.php");
+    $username = $_SESSION['username'];
+    $conn->query("UPDATE login SET status=0 where username='$username'");
+
     session_unset();
     session_destroy();
     header("Location:login.php");
